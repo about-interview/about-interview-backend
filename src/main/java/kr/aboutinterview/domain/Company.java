5a77e309ac8ef4a964dc605327f520da6df196eb
manijang2 <manijang2@naver.com>
2022-01-21 00:30:01 +0900

    [add] CORS 적용

--- src/main/java/kr/aboutinterview/domain/Company.java
+++ src/main/java/kr/aboutinterview/domain/Company.java
@@ -5,17 +5,15 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Getter // get 함수를 일괄적으로 만들어줍니다.
-@NoArgsConstructor // 기본 생성자를 만들어줍니다.
-@Entity // DB 테이블 역할을 합니다.
+@Getter
+@NoArgsConstructor
+@Entity
 public class Company {
 
-    // ID가 자동으로 생성 및 증가합니다.
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long idx;
 
-    // 반드시 값을 가지도록 합니다.
     @Column(nullable = false)
     private String name;
 
